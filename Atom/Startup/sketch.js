1e34c6bde8e1fc9034150ac8ed9a861482bcb41d
797979-SG <sebasconde1103@gmail.com>
2021-03-01 14:42:57 -0800

    Initial commit

---
+++ Atom/Startup/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);
+}
+
+//  The draw function is called @ 30 fps
+function draw(){
+}
